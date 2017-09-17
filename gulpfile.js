5c599d023cb55d46c2862bdfd64eb2798a2eb6c2
Max Höffner <maxhoeffner@aol.com>
2017-09-17 17:24:34 +0200

    improve functionaliy // rm lint errors
    * properly config `suitcss-preprocessor` to accept `xl-` viewport-prefix
    * remove all lint errors so by defualt the linter runs smoothly
    * add color `postcss-color-function`
    * use vars to build spacers
    * use vars for font-weight

--- gulpfile.js
+++ gulpfile.js
@@ -20,13 +20,21 @@ gulp.task('javascript', () => {
         .pipe(gulp.dest('dist'))
 })
 
-gulp.task('suitcss', () =>
+// exteded suit css watching
+gulp.task('suitcss', () => {
     gulp.src('src/css/index.css')
         .pipe(suitcss({
             minify: true,
+            use: ['postcss-color-function'],
+            'postcss-bem-linter': {
+                componentName: "^[A-Z][a-zA-Z0-9]+$",
+                componentSelectors: suitSelector,
+                utilitySelectors: "^\.u-(sm-|md-|lg-|xl-)?(?:[a-z0-9][a-zA-Z0-9]*)+$",
+            }
         }))
         .pipe(gulp.dest('dist'))
-);
+});
+
 // Watch Files For Changes
 gulp.task('watch', function () {
     gulp.watch('src/css/**/*.css', ['suitcss']);
@@ -35,3 +43,21 @@ gulp.task('watch', function () {
 
 // Default Task
 gulp.task('default', ['suitcss', 'javascript', 'watch']);
+
+/*
+    extend default suit linting
+    https://github.com/postcss/postcss-bem-linter/blob/master/lib/preset-patterns.js
+*/
+function suitSelector(componentName, presetOptions) {
+    const ns = (presetOptions && presetOptions.namespace) ? `${presetOptions.namespace}-` : '';
+    const WORD = '[a-z0-9][a-zA-Z0-9]*';
+    const descendant = `(?:-${WORD})?`;
+    const modifier = `(?:--${WORD}(?:\\.${ns}${componentName}${descendant}--${WORD})*)?`;
+    const attribute = '(?:\\[.+\\])?';
+    const state = `(?:\\.is-${WORD})*`;
+    const body = descendant +
+        modifier +
+        attribute +
+        state;
+    return new RegExp(`^\\.${ns}${componentName}\\b${body}$`);
+}
